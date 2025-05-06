178798410b95b4dbd6bde1828fca112796bbbb55
halkbank <halkbank@DESKTOP-E3RPGM2>
2025-05-06 15:27:01 +0300

    async

---
+++ Console_Async/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+
+
+void Islem1()
+{
+    Console.WriteLine("Islem 1");
+    Thread.Sleep(5000);
+    Console.WriteLine("Islem 1 bitti");
+}
+void Islem2()
+{
+    Console.WriteLine("Islem 2");
+    Thread.Sleep(8000);
+    Console.WriteLine("Islem 2 bitti");
+}
+void Islem3()
+{
+    Console.WriteLine("Islem 3");
+    Thread.Sleep(7000);
+    Console.WriteLine("Islem 3 bitti");
+}
+
+// Senkron
+//Stopwatch stopwatch = new Stopwatch();
+//stopwatch.Start();
+
+//Islem1();
+//Islem2();
+//Islem3();
+
+//stopwatch.Stop();
+//Console.WriteLine("Gecen süre = " + stopwatch.ElapsedMilliseconds);
+
+
+
+async Task Islem1Async()
+{
+    Console.WriteLine("Islem 1");
+     await Task.Delay(5000);
+    Console.WriteLine("Islem 1 bitti");
+}
+async Task Islem2Async()
+{
+    Console.WriteLine("Islem 2");
+    await Task.Delay(8000);
+    Console.WriteLine("Islem 2 bitti");
+}
+async Task Islem3Async()
+{
+    Console.WriteLine("Islem 3");
+    await Task.Delay(7000);
+    Console.WriteLine("Islem 3 bitti");
+}
+
+// Asenkron
+Stopwatch stopwatch = new Stopwatch();
+stopwatch.Start();
+
+Task.WaitAll(
+    Islem1Async(),
+    Islem2Async(),
+    Islem3Async()
+);
+
+stopwatch.Stop();
+Console.WriteLine("Gecen süre = " + stopwatch.ElapsedMilliseconds);
